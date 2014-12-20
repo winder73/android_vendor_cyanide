211dbb050cbf9d147be20b77eb82cf4047d06e2d
rogersb11 <brettrogers11@gmail.com>
2014-12-19 21:57:28 -0500

    Add i777
    Change-Id: I63e947f5c63bd2273158c8096df3c4216d4330dc

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 for combo in $(curl -s https://raw.githubusercontent.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-11.0 | awk {'print $1'})
 do
     add_lunch_combo $combo
+    add_lunch_combo cm_i777-userdebug
 done
