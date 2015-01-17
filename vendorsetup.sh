0c5916b94fa0271df8b16b8e580791a29cfb27a0
rogersb11 <brettrogers11@gmail.com>
2015-01-17 00:44:06 -0500

    Add back t0lte and d2att
    Change-Id: I789714027ba13f0cd1e63effced42349b9d6cb04

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,4 +3,6 @@ do
     add_lunch_combo $combo
     add_lunch_combo cm_i777-userdebug
     add_lunch_combo cm_m8-userdebug
+    add_lunch_combo cm_t0lte-userdebug
+    add_lunch_combo cm_d2att-userdebug
 done
