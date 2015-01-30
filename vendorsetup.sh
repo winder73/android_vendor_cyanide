828a089caa7efcbea3261186623c5684544643df
Jlmancuso <jlmancuso1134@gmail.com>
2015-01-29 18:09:04 -0600

    remove auto add lunch combo's

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,4 @@
-for combo in $(curl -s https://raw.githubusercontent.com/CyanideL/hudson/master/cyanide-build-targets | sed -e 's/#.*$//' | grep cm-12.0 | awk {'print $1'})
-do
-    add_lunch_combo $combo
     add_lunch_combo cyanide_i777-userdebug
     add_lunch_combo cyanide_m8-userdebug
     add_lunch_combo cyanide_t0lte-userdebug
     add_lunch_combo cyanide_d2att-userdebug
-done
